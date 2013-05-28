3934a01e86da7366f1a832d9423bf83ae17de631
Valerchik <valeriy-v@yandex.ru>
2013-05-28 16:46:31 +0300

    Fixed syntax errors. Added 1s timeouts by default.

--- scripts/client.php
+++ scripts/client.php
@@ -14,7 +14,7 @@ $opts = getopt('n:u:');
 $n = isset($opts['n']) ? (int)$opts['n'] : 1;
 $n = $n < 0 ? 1 : $n;
 $url = isset($opts['u']) ? $opts['u'] : 'tcp://localhost:5599';
-$timeout = isset($opts['t']) ? (int)$opts['t'] : 100;
+$timeout = isset($opts['t']) ? (int)$opts['t'] : 1000;
 
 $context = new \ZMQContext();
 $socket = new \ZMQSocket($context, \ZMQ::SOCKET_REQ);
