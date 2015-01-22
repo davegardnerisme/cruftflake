818714717e98f409078633fd2fbeabb938dfca6a
Lucas Vasconcelos <lucas.vasconcelos@dialoga.com.br>
2015-01-22 16:56:19 +0000

    side effect removed from methods getMachine()

--- src/Davegardnerisme/CruftFlake/FixedConfig.php
+++ src/Davegardnerisme/CruftFlake/FixedConfig.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Fixed configuration
- * 
+ *
  * This is designed to be used where each machine **knows** what its machine
  * ID is - eg: via some kind of automatically deployed configuration
  * (puppet etc.)
- * 
+ *
  * @author @davegardnerisme
  */
 
@@ -15,29 +15,29 @@ class FixedConfig implements ConfigInterface
 {
     /**
      * Machine ID
-     * 
+     *
      * @var integer
      */
     private $machineId;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param integer $machineId
      */
     public function __construct($machineId)
     {
         $this->machineId = (int)$machineId;
     }
-    
+
     /**
      * Get machine identifier
-     * 
+     *
      * @return integer Should be a 10-bit int (decimal 0 to 1023)
      */
     public function getMachine()
     {
-        echo "Claimed machine ID {$this->machineId} via fixed configuration.\n";
+        // echo "Claimed machine ID {$this->machineId} via fixed configuration.\n";
 
         return $this->machineId;
     }
--- src/Davegardnerisme/CruftFlake/ZkConfig.php
+++ src/Davegardnerisme/CruftFlake/ZkConfig.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * ZooKeeper-based configuration
- * 
+ *
  * Couple of points:
- * 
+ *
  *  1. We coordinate via ZK on launch - hence ZK must be available at launch
  *     time
  *  2. We create permanent nodes (not ephmeral) so that if we get disconnected
@@ -13,7 +13,7 @@
  *  4. This is assuming we don't run > 1 server on the same box - which we
  *     won't be able to do anyway since we bind to a ZeroMQ TCP port (which
  *     we can only do once)
- * 
+ *
  * @author @davegardnerisme
  */
 
@@ -23,21 +23,21 @@ class ZkConfig implements ConfigInterface
 {
     /**
      * Parent path
-     * 
+     *
      * @var string
      */
     private $parentPath;
-    
+
     /**
      * ZK
-     * 
+     *
      * @var \Zookeeper
      */
     private $zk;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param string $hostnames A comma separated list of hostnames (including
      *      port)
      * @param string $zkPath The ZK path we look to find other machines under
@@ -52,21 +52,21 @@ class ZkConfig implements ConfigInterface
         $this->zk = new \Zookeeper($hostnames);
         $this->parentPath = $zkPath;
     }
-    
+
     /**
      * Get machine identifier
-     * 
+     *
      * @return integer Should be a 10-bit int (decimal 0 to 1023)
      */
     public function getMachine()
     {
         $machineId = NULL;
-        
+
         $this->createParentIfNeeded($this->parentPath);
-     
+
         // get info about _this_ machine
         $machineInfo = $this->getMachineInfo();
-        
+
         // get current machine list
         $children = $this->zk->getChildren($this->parentPath);
         foreach ($children as $child) {
@@ -76,7 +76,7 @@ class ZkConfig implements ConfigInterface
                 $machineId = (int)$child;
             }
         }
-        
+
         // find an unused machine number
         for ($i=0; $i<1024, $machineId === NULL; $i++) {
             $machineNode = $this->machineToNode($i);
@@ -99,27 +99,27 @@ class ZkConfig implements ConfigInterface
                 break;
             }
         }
-        
+
         if ($machineId === NULL) {
             throw new \RuntimeException(
                     "Cannot locate and claim a free machine ID via ZK"
                     );
         }
-        
-        echo "Claimed machine ID {$machineId} for " . json_encode($machineInfo) . "\n";
-        
+
+        // echo "Claimed machine ID {$machineId} for " . json_encode($machineInfo) . "\n";
+
         return (int)$machineId;
     }
-    
+
     /**
      * Get mac address and hostname
-     * 
+     *
      * @return array "macAddress", "hostname" keys
      */
     private function getMachineInfo()
     {
         $info = array();
-        // HWaddr 12:31:3c:01:65:b8 
+        // HWaddr 12:31:3c:01:65:b8
         // ether 00:1c:42:00:00:08
         exec('ifconfig', $output);
         foreach ($output as $o) {
@@ -129,7 +129,7 @@ class ZkConfig implements ConfigInterface
             }
         }
         $info['hostname'] = exec('hostname');
-        
+
         if (empty($info['hostname']) || empty($info['macAddress'])) {
             throw new \RuntimeException(
                     'Unable to identify machine mac address and hostname'
@@ -137,10 +137,10 @@ class ZkConfig implements ConfigInterface
         }
         return $info;
     }
-    
+
     /**
      * Create parent node, if needed
-     * 
+     *
      * @param string $nodePath
      */
     private function createParentIfNeeded($nodePath)
@@ -160,9 +160,9 @@ class ZkConfig implements ConfigInterface
 
     /**
      * Machine ID to ZK node
-     * 
+     *
      * @param integer $id
-     * 
+     *
      * @return string The node path to use in ZK
      */
     private function machineToNode($id)
